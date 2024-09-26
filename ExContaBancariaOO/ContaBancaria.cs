4186019008414db3aad58fe27237aaa6922795e5
Guilherme Renato Terra De Macedo <guirenato1181@gmail.com>
2024-09-26 08:18:29 -0300

    Adicionar arquivos de projeto.

---
+++ ExContaBancariaOO/ContaBancaria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExContaBancariaOO {
+    internal class ContaBancaria {
+
+        //Encapsulamento dos meus atributos
+        public int NumeroConta { get; private set; } //Numero não pode ser alterado
+        public string NomeTitular { get; set; } //Nome pode ser alterado
+        public double Saldo { get; private set; } //Saldo só pode ser alterado por meio de deposito/saque
+
+        //Vou ter um construtor com 3 argumentos e outro com 2 => sobrecarga
+        public ContaBancaria(int numero, string titular) {
+            NumeroConta = numero;
+            NomeTitular = titular;
+            Saldo = 0.0;
+        }
+
+        //utilizar o this para reaproveitar os dois comandos de cima
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular){
+            Deposito(depositoInicial); //Nao precisa reimplementar a lógica, só chamar o método
+        }
+
+        public void Deposito(double quantia) {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia) {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroConta
+                + ", Titular: "
+                + NomeTitular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExContaBancariaOO/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExContaBancariaOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+            if(resposta == 's')
+            {
+                Console.WriteLine("Entre com o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor do depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor do saque: ");
+            double quantiaSaque = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+            conta.Saque(quantiaSaque);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
